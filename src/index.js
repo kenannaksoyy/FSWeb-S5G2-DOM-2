c0a57e451f54817ab80227eca5fb9c6ab2653d17
Kenan AKSOY <kenannaksoyy@gmail.com>
2022-11-22 21:57:15 +0300

    Kenan AKSOY

--- src/index.js
+++ src/index.js
@@ -7,3 +7,25 @@ import './less/index.less'
 
 
 // Kodlar buraya gelecek!
+function random(){
+    return Math.floor(Math.random()) * 256;
+}
+document.querySelector("h1").addEventListener("click",(e)=>{
+    alert("Bana Tıkladın");    
+});
+let baslik=document.querySelector("h1");
+baslik.addEventListener("mouseover", (e)=>{
+    e.target.style.color="red";
+});
+baslik.addEventListener("mouseout", (e)=>{
+    e.target.style.color="green";
+});
+document.querySelector(".main-navigation").addEventListener("keydown",(e)=>{
+    e.target.style.backgroundColor=`rgb(${random()}, ${random()}, ${random()})`;
+});
+document.querySelector(".main-navigation").addEventListener("wheel",(e)=>{
+    e.target.style.color=`rgb(${random()}, ${random()}, ${random()})`;
+});
+document.querySelector(".intro").addEventListener("focus" , (e) => {
+    e.target.style.backgroundColor = "red";
+});
